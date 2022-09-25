1f848b0f5044b1df6febb9ae6f380eaa6f46daff
brenda.moura <brenda.moura@Plugifys-MacBook-Pro.local>
2022-09-24 21:36:39 -0400

    Adding search bar

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,56 @@ function App() {
     });
   }, [state.word]);
 
-  return <Container></Container>;
+  return (
+    <Container
+      sx={{
+        width: "80%",
+        height: "100%",
+        margin: "auto",
+        marginTop: "3rem",
+        backgroundColor: "#FAFAFB",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ width: "30%" }}>
+        Lucy Dictictionary: a better yourself in one click.
+      </h1>
+      <TextField
+        sx={{ marginTop: "2rem", width: "50%" }}
+        id="search word"
+        variant="outlined"
+        value={state?.word}
+        onChange={handleChange}
+      />
+      <Box
+        sx={{
+          width: "100%",
+          marginTop: "3rem",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+        }}
+      >
+        <h2>Word of the day</h2>
+        <Box sx={{ width: "100%", display: "flex", flexWrap: "wrap" }}>
+          {state.results?.map((word) => (
+            <Box
+              sx={{
+                width: "100px",
+                height: "100px",
+                backgroundColor: "lightblue",
+              }}
+            >
+              {word.definition}
+            </Box>
+          ))}
+        </Box>
+      </Box>
+    </Container>
+  );
 }
 
 export default App;
