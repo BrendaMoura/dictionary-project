9795f5ae258014c51246587080cfdb9cf294866a
brenda.moura <brenda.moura@Plugifys-MacBook-Pro.local>
2022-09-25 21:39:59 -0400

    Adding card recommendation

---
+++ src/components/CardRecomendation/index.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/system";
+import React, { FC } from "react";
+import { WordInfo } from "../../typings";
+
+interface Props {
+  info: WordInfo;
+}
+
+const CardRecomendation: FC<Props> = ({ info }) => {
+  const firstMeaning = info.results ? info.results[0].definition : "";
+  return (
+    <Box
+      sx={{
+        width: "200px",
+        height: "200px",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "lightblue",
+      }}
+    >
+      <h2>{info.word}</h2>
+      <p>{info.pronunciation?.all}</p>
+      <p>{firstMeaning}</p>
+    </Box>
+  );
+};
+
+export default CardRecomendation;
